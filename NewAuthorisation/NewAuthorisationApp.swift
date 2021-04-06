216ec24aad5375757288cd6168160d97632ed39b
Olesia Maslennikova <olesya_korovina_1993@mail.ru>
2021-04-06 23:56:03 +0300

    New authorisation project

---
+++ NewAuthorisation/NewAuthorisationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewAuthorisationApp.swift
+//  NewAuthorisation
+//
+//  Created by user on 06.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewAuthorisationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
